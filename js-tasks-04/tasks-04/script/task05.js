dfe1b84ff4716e7eea7088f1fe3b7f4083ae49f1
ChrisGloom1 <89384429+ChrisGloom1@users.noreply.github.com>
2022-09-09 18:16:50 +0200

    Created skeleton for task05 as well as some basic js

---
+++ js-tasks-04/tasks-04/script/task05.js
@@ -0,0 +1,44 @@
+const employeeArray = [
+  {
+    firstName: "André",
+    surname: "Sørensen",
+    department: "HR"
+  },
+  {
+    firstName: "Tonje",
+    surname: "Nordquist",
+    department: "UX"
+  },
+  {
+    firstName: "Chris",
+    surname: "Glommen",
+    department: "Utvikler"
+  },
+  {
+    firstName: "Steffen",
+    surname: "Johansen",
+    department: "HR"
+  },
+  {
+    firstName: "Celina",
+    surname: "Vimme",
+    department: "UX"
+  },
+  {
+    firstName: "Fredrik",
+    surname: "Kåsin",
+    department: "Utvikler"
+  }
+]
+
+const departmentSelector = document.querySelector( "#department-selector" );
+const searchBtn = document.querySelector( "#search-btn" );
+const departmentOutput = document.querySelector( "#department-output" );
+let capitalFilter = departmentSelector.filter(departmentSelector.value = )
+
+const showDepartment = () => {
+  
+}
+
+
+searchBtn.addEventListener( "click", showDepartment )
