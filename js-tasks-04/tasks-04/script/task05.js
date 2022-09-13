d98119f905690b26e7aca36c893c5b4bb14c7605
Christopher Glommen Andresen <89384429+ChrisGloom1@users.noreply.github.com>
2022-09-13 16:13:18 +0200

    tasks-04, task05 completed

--- js-tasks-04/tasks-04/script/task05.js
+++ js-tasks-04/tasks-04/script/task05.js
@@ -31,27 +31,31 @@ const employeeArray = [
   }
 ];
 
+const getUtvikler = employeeArray.filter(title => title.department == "Utvikler");
+const getUx = employeeArray.filter(title => title.department == "ux");
+const getHr = employeeArray.filter(title => title.department == "HR");
+
 const departmentSelector = document.querySelector( "#department-selector" );
 const searchBtn = document.querySelector( "#search-btn" );
 const departmentOutput = document.querySelector( "#department-output" );
 
-const checkSelectValue = () => {
-  let department = departmentSelector.value;
-  switch (department){
-    case "utvikler":
-      printUtvikler();
-      break;
-    case "ux":
-      printUx();
-      break;
-    case "hr":
-      printHr();
-      break;
-  }
-}
 
-printUtvikler = () => console.log("utvikler")
-printUx = () => console.log("ux")
-printHr = () => console.log("hr")
+const checkSelectValue = () => {
+  let htmlTxt = "";
+  let department = departmentSelector.value;
+  let chosenEmployees = employeeArray.filter(employee =>
+      employee.department === department
+    ) 
+    chosenEmployees.forEach(employee => {
+      htmlTxt += `
+      <article>
+        <h3>${employee.firstName} ${employee.surname}</h3>
+        <p>${employee.department}</p>
+      </article>
+      `;
+    })
+    departmentOutput.innerHTML = "";
+    departmentOutput.innerHTML = htmlTxt;
+}
 
 searchBtn.addEventListener( "click", checkSelectValue )
