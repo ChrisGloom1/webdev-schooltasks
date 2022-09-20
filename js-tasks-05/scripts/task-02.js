2f76b1f59428aa6c20c56050c81d6a18935ff8fd
Christopher Glommen Andresen <89384429+ChrisGloom1@users.noreply.github.com>
2022-09-20 16:09:04 +0200

    Completed task 05-02

--- js-tasks-05/scripts/task-02.js
+++ js-tasks-05/scripts/task-02.js
@@ -1,4 +1,41 @@
-import PizzaModule from "./modules/PizzaModule.js";
+const diavolaBtn = document.querySelector( "#diavola-btn" );
+const finocchionaBtn = document.querySelector( "#finocchiona-btn" );
+const divOutputShoppingCart = document.querySelector( "#div-output-shopping-cart" );
+const shoppingCartArray = [];
+
+const addDiavolaToShoppingCart = () => {
+  shoppingCartArray.push( { name: "Diavola", price: 199 } );
+  showShoppingCart();
+};
+
+const addFinocchionaToShoppingCart = () => {
+  shoppingCartArray.push ( { name: "Finocchiona", price: 169 } );
+  showShoppingCart();
+};
+
+const showShoppingCart = () => {
+  if ( shoppingCartArray.length > 0 ){
+    let htmlTxt = ""; 
+    shoppingCartArray.forEach( pizza => {
+      htmlTxt += `
+      <article>
+        <h3>${pizza.name}</h3>
+        <p>${pizza.price}</p>
+      </article>
+      `;
+    })
+    divOutputShoppingCart.innerHTML = `
+    <h4>Totalpris: ${ getTotalPrice() }</h4>
+    ${ htmlTxt }
+    `;
+  } else {
+    divOutputShoppingCart.innerHTML = "Handlekurven din er tom.";
+  }
+}
 
 
-console.log(getAllPizzas)
+const getTotalPrice = () => shoppingCartArray.reduce( ( acc, curr ) => acc + curr.price, 0 );
+
+showShoppingCart();
+diavolaBtn.addEventListener( "click", addDiavolaToShoppingCart );
+finocchionaBtn.addEventListener( "click", addFinocchionaToShoppingCart );
