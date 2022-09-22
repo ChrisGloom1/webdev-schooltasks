68e46b346d2dc964c6cbe7eb9d1aedb4f1c87afc
ChrisGloom1 <89384429+ChrisGloom1@users.noreply.github.com>
2022-09-22 11:29:21 +0200

    Changed variable name so it makes more sense

--- js-tasks-06/scripts/my-style.js
+++ js-tasks-06/scripts/my-style.js
@@ -2,19 +2,16 @@ const btnChangeStyling = document.querySelector( "#btn-change-styling" );
 
 const setStyleFromLocalStorage = () => {
   const arrStyling = [];
-  const bgClrInLS = localStorage.getItem( "newStyle" );
-  const bgClrInLSParsed = JSON.stringify( bgClrInLS );
-  arrStyling.push( bgClrInLSParsed );
+  const stylingFromLS = localStorage.getItem( "newStyle" );
+  const stylingFromLSParsed = JSON.stringify( stylingFromLS );
+  arrStyling.push( stylingFromLSParsed );
   console.log( arrStyling );
   document.body.style.color = arrStyling.txtClr;
   document.body.style.backgroundColor = arrStyling.bgClr;
   console.log( arrStyling.txtClr );
   console.log( arrStyling.bgClr );
-
 };
 
 // ( () => setStyleFromLocalStorage() ) ();
 
 btnChangeStyling.addEventListener( "click", setStyleFromLocalStorage );
-
-
