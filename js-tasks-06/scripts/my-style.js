7e6d0ccadc50c5a6c6049b114712ccf8be658315
Christopher Glommen Andresen <89384429+ChrisGloom1@users.noreply.github.com>
2022-09-22 15:20:58 +0200

    Task 06-02 complete

--- js-tasks-06/scripts/my-style.js
+++ js-tasks-06/scripts/my-style.js
@@ -1,17 +1,7 @@
-const btnChangeStyling = document.querySelector( "#btn-change-styling" );
-
 const setStyleFromLocalStorage = () => {
-  const arrStyling = [];
-  const stylingFromLS = localStorage.getItem( "newStyle" );
-  const stylingFromLSParsed = JSON.stringify( stylingFromLS );
-  arrStyling.push( stylingFromLSParsed );
-  console.log( arrStyling );
-  document.body.style.color = arrStyling.txtClr;
-  document.body.style.backgroundColor = arrStyling.bgClr;
-  console.log( arrStyling.txtClr );
-  console.log( arrStyling.bgClr );
+  const stylingFromLSParsed = JSON.parse( localStorage.getItem( "newStyle" ) );
+  document.body.style.color = stylingFromLSParsed[0].txtClr;
+  document.body.style.backgroundColor = stylingFromLSParsed[0].bgClr;
 };
 
-// ( () => setStyleFromLocalStorage() ) ();
-
-btnChangeStyling.addEventListener( "click", setStyleFromLocalStorage );
+( () => setStyleFromLocalStorage() ) ();
