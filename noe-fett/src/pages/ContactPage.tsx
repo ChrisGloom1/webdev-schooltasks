e523c05e465af357ea5511536a94df507d3871b6
ChrisGloom1 <89384429+ChrisGloom1@users.noreply.github.com>
2022-10-20 16:09:56 +0200

    another restructure of the folder

--- noe-fett/src/pages/ContactPage.tsx
+++
@@ -1,9 +0,0 @@
-const ContactPage = () => {
-  return(
-    <section>
-      <h1>Konntakt sidn vår hehe</h1>
-    </section>
-  )
-}
-
-export default ContactPage;
--- noe-fett/src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-import ContactPage from "./ContactPage";
-import HomePage from "./HomePage";
-import VehiclesPage from "./VehiclesPage";
-
-export { ContactPage, HomePage, VehiclesPage }
